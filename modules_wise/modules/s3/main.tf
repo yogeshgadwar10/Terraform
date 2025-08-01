613a0ca7176024e32574d3bf06174d58b5f6b0e5
57549_YOGESH <yogeshgadwar@gmail.com>
2025-08-01 18:30:33 +0530

    s3

--- modules_wise/modules/s3/main.tf
+++ modules_wise/modules/s3/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
 
   versioning_configuration {
-    status = var.enable_versioning ? "Enabled" : "Suspended"
+    status = var.enable_versioning ? "Enabled" : "Suspended"   
   }
 }
 
