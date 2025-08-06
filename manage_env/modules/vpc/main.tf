7f4bfb09b323c0f0f6c07eb082f534b8cbe08e95
57549_YOGESH <yogeshgadwar@gmail.com>
2025-08-06 14:27:09 +0530

    fix

--- manage_env/modules/vpc/main.tf
+++ manage_env/modules/vpc/main.tf
@@ -3,41 +3,40 @@ resource "aws_vpc" "my_vpc" {
     tags = {
         Name = "${var.project}-vpc"
         env = var.env
-    }  
+    }
 }
 
 resource "aws_subnet" "pri_sub" {
     cidr_block = var.pri_sub_cidr
     tags = {
-      Name = "${var.project}-pri-subnet"
-      env = var.env
+        Name = "${var.project}-pri-subnet"
+        env = var.env
     }
-    vpc_id = aws_vpc.my_vpc.id 
+    vpc_id = aws_vpc.my_vpc.id
 }
 
-resource "aws_subnet" "pub_sub_sub" {
+resource "aws_subnet" "pub_sub" {
     cidr_block = var.pub_sub_cidr
     tags = {
-      Name = "${project}-pub-subnet"
-      env = var.env
+        Name = "${var.project}-pub-subnet"
+        env = var.env
     }
-    vpc_id = aws_vpc.my_vpc.id 
+    vpc_id = aws_vpc.my_vpc.id
     map_public_ip_on_launch = true
     availability_zone = var.az
-
 }
 
 resource "aws_internet_gateway" "my_igw" {
     vpc_id = aws_vpc.my_vpc.id
     tags = {
-      Name = "${project}-igw"
-    } 
+        Name = "${var.project}-igw"
+    }
 }
 
 resource "aws_default_route_table" "my_rt" {
     default_route_table_id = aws_vpc.my_vpc.default_route_table_id
-    tags = {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.my_igw.id
-    }  
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my_igw.id
+    }
 }
